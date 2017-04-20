c09d6f58b58ed936a5c7cd8074a92dd056768d13
Ronan <ronanlet@gmail.com>
2017-04-20 10:54:33 +0200

    Uncommented test CORS

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.use(session({
   }
 }))
 
-// CORS
-/* app.use(function (req, res, next) {
+// CORS (for testing)
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
-}) */
+})
 
 app.engine('js', (path, item, cb) => {
   const template = require(`${path}`)
